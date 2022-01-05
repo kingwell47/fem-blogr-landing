683a6c0f9529bb9af6fe86ccea9af2441711ea34
kingwell47 <joeldoctor47@gmail.com>
2022-01-05 17:49:56 +0800

    adding some components initially

--- src/components/AppNavBar.jsx
+++ src/components/AppNavBar.jsx
@@ -4,6 +4,10 @@ const AppNavBar = () => {
   return (
     <div>
       <h1>NavBar</h1>
+      <div>
+        Product Overview Pricing Marketplace Features Integrations Company About
+        Team Blog Careers Connect Contact Newsletter LinkedIn Login Sign Up
+      </div>
     </div>
   );
 };
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div>
+      A modern publishing platform Grow your audience and build your online
+      brand Start for Free Learn More
+    </div>
+  );
+};
+
+export default HeroSection;
