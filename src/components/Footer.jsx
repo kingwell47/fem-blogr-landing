f38b5f8cc73e4c83947294a936ec4e18338228e2
kingwell47 <joeldoctor47@gmail.com>
2022-01-07 16:41:10 +0800

    added more components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      Product Overview Pricing Marketplace Features Integrations Company About
+      Team Blog Careers Connect Contact Newsletter LinkedIn
+    </div>
+  );
+};
+
+export default Footer;
